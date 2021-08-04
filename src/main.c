22e6497b567efe37d52c6d95e6de9eaff3a7ccd9
u-haru <40634644+u-haru@users.noreply.github.com>
2021-08-04 13:32:02 +0900

    全キー対応、大幅に簡略化 バッファクリアの手法変更

--- src/main.c
+++ src/main.c
@@ -1,49 +1,29 @@
 #include <windows.h>
-#include <mmsystem.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
 void exitfunc(){
-    printf("Exit now...");
-    fflush(stdout);
-    fflush(stdin);
+    printf("Exit now...\n");
+	char c;
+    while ((c = getchar()) != EOF){}//バッファクリア
 }
 int main()
 {
     atexit(exitfunc);
-	bool key[46];
+	bool key[0xff];
 	memset(key,0,sizeof(key));
 	while (true)
 	{
-		for (int i = 48; i <= 90; i++) {
-
-			if (GetAsyncKeyState(i) && key[i-48] == FALSE)
-			{
-				printf("%c",(char)i);
-				PlaySound("Sounds/KEYS",0, SND_FILENAME | SND_ASYNC);
-				key[i - 48] = TRUE;
-			}else if(GetAsyncKeyState(i) == FALSE) key[i - 48] = FALSE;
+		for(int i = 0; i < 0xff; i++) {
+			if(GetAsyncKeyState(i) && !key[i]){
+				key[i] = true;
+				switch(i){
+					case VK_SPACE: PlaySound("Sounds/SPACE.wav", 0, SND_FILENAME | SND_ASYNC);break;
+					case VK_RETURN: PlaySound("Sounds/ENTER.wav", 0, SND_FILENAME | SND_ASYNC);break;
+					default: PlaySound("Sounds/KEYS.wav", 0, SND_FILENAME | SND_ASYNC);break;
+				}
+			}else if(!GetAsyncKeyState(i)) key[i] = false;
 		}
-		if (GetAsyncKeyState(VK_OEM_MINUS) && key[45] == FALSE)
-		{
-			printf("-");
-			PlaySound("Sounds/KEYS.wav", 0, SND_FILENAME | SND_ASYNC);
-			key[45] = TRUE;
-		}else if(GetAsyncKeyState(VK_OEM_MINUS) == FALSE) key[45] = FALSE;
-		if (GetAsyncKeyState(VK_RETURN) && key[43] == FALSE)
-		{
-			printf("\n");
-			PlaySound("Sounds/ENTER.wav", 0, SND_FILENAME | SND_ASYNC);
-			key[43] = TRUE;
-		}else if (GetAsyncKeyState(VK_RETURN) == 0) key[43] = FALSE;
-		if (GetAsyncKeyState(VK_SPACE) && key[44] == FALSE)
-		{
-			printf(" ");
-			PlaySound("Sounds/SPACE.wav", 0, SND_FILENAME | SND_ASYNC);
-			key[44] = TRUE;
-		}
-		else if (GetAsyncKeyState(VK_SPACE) == 0) key[44] = FALSE;
 	}
 	return 0;
 }
